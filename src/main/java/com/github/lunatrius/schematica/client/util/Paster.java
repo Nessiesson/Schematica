b04c924a0d9589ef972655ad3c7a8b1f3b4906a1
robitobi01 <robitobi01@gmx.de>
2020-04-28 23:27:24 +0200

    Some fix idea

--- src/main/java/com/github/lunatrius/schematica/client/util/Paster.java
+++ src/main/java/com/github/lunatrius/schematica/client/util/Paster.java
@@ -5,6 +5,7 @@ import com.github.lunatrius.core.util.math.MBlockPos;
 import com.github.lunatrius.schematica.block.state.BlockStateHelper;
 import com.github.lunatrius.schematica.client.world.SchematicWorld;
 import com.github.lunatrius.schematica.reference.Names;
+import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
@@ -29,10 +30,10 @@ public class Paster {
         return pasteAir;
     }
 
-    private static boolean setBlockAt(final IBlockState blockState, final BlockPos pos, final World world, final boolean canPlaceBlockAt) {
+    private static boolean setBlockAt(final IBlockState blockState, final BlockPos pos, final World world) {
         Block block = blockState.getBlock();
         boolean successfull = false;
-        if(!canPlaceBlockAt || block.canPlaceBlockAt(world, pos)) {
+        if(block.canPlaceBlockAt(world, pos)) {
             if (pos.getY() > 0 && pos.getY() < 256 && world.isBlockLoaded(pos)) {
                 String cmd = String.format("%d %d %d %s %d", pos.getX(), pos.getY(), pos.getZ(), block.getRegistryName(), block.getMetaFromState(blockState));
                 world.sendPacketToServer(new CPacketChatMessage("/setblock " + cmd + " destroy"));
@@ -44,30 +45,25 @@ public class Paster {
 
     public static void paste(final EntityPlayer player, final SchematicWorld world, final World mcWorld) {
         int count = 0;
-        List<MBlockPos> placeAfterwards = new ArrayList<>();
+        int errorCount = 0;
+        List<MBlockPos> blocksToPlace = Lists.newArrayList(BlockPosHelper.getAllInBox(BlockPos.ORIGIN, new BlockPos(world.getWidth() - 1, world.getHeight() - 1, world.getLength() - 1)));
         mcWorld.sendPacketToServer(new CPacketChatMessage("/gamerule sendCommandFeedback false"));
         mcWorld.sendPacketToServer(new CPacketChatMessage("/gamerule doTileDrops false"));
-        for (final MBlockPos pos : BlockPosHelper.getAllInBox(BlockPos.ORIGIN, new BlockPos(world.getWidth() - 1, world.getHeight() - 1, world.getLength() - 1))) {
+        while (blocksToPlace.size() > 0) {
+            MBlockPos pos = blocksToPlace.get(0);
+            blocksToPlace.remove(0);
             final IBlockState blockState = world.getBlockState(pos);
             final Block block = blockState.getBlock();
             final BlockPos mcPos = new BlockPos(world.position.add(pos));
-            if (!BlockStateHelper.areBlockStatesEqual(blockState, mcWorld.getBlockState(mcPos)) && mcPos.getY() > 0 && mcPos.getY() < 256 && mcWorld.isBlockLoaded(mcPos)) {
+            if (!BlockStateHelper.areBlockStatesEqual(blockState, mcWorld.getBlockState(mcPos))) {
                 if (pasteAir || !(block.isAir(blockState, mcWorld, mcPos))) {
-                    if (setBlockAt(blockState, mcPos, mcWorld, true)) {
+                    if (setBlockAt(blockState, mcPos, mcWorld)) {
                         count++;
                     } else {
-                        placeAfterwards.add(new MBlockPos(pos));
+                        errorCount++;
+                        blocksToPlace.add(blocksToPlace.size() -1, new MBlockPos(pos));
                     }
                 }
-            }
-        }
-        // Try to placed failed blocks 2x to prevent broken pastes
-        for (int i = 0; i < 2; i++) {
-            for (MBlockPos pos : placeAfterwards) {
-                final IBlockState blockState = world.getBlockState(pos);
-                final BlockPos mcPos = new BlockPos(world.position.add(pos));
-                setBlockAt(blockState, mcPos, mcWorld, false);
-                count++;
             }
         }
         mcWorld.sendPacketToServer(new CPacketChatMessage("/gamerule doTileDrops true"));
